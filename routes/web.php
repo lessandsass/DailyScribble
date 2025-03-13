7766873c395746bc8781e375bc206d3b87b0f93f
Vipu <lessandsass@gmail.com>
2025-03-13 15:46:27 +0530

    The Dashboard page
    - The dashboard.blade.php file was created inside the views directory by duplicating the home.blade.php file.
    - The *DashboardController*  was created by using the artisan command inside the app\Http\Controllers directory
    - The dashboard route was created inside the web.php file by using the controller
    - The index method was created inside the DashboardController

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
 });
+
+Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
