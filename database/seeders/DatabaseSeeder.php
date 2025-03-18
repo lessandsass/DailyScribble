2523465de1d3d4962eddf6798bc82f3166e28481
Vipu <lessandsass@gmail.com>
2025-03-19 02:24:11 +0530

    Call the classes as an array
    - We can call the classes as an array if you want call multiple classes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,9 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
 
-        $this->call(UserSeeder::class);
+        $this->call([
+            UserSeeder::class
+        ]);
 
     }
 }
